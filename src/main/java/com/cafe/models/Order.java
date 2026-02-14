219e77077b0553c7fed92ef77c8d5891e6ff54f5
Lakshmi <sripriyapalli@gmail.com>
2026-02-14 13:03:11 -0500

    Create Order class to manage customer orders

---
+++ src/main/java/com/cafe/models/Order.java
@@ -0,0 +1,39 @@
+//Keep track of Orders
+//Connect customer with Order (Drink) - Inventory Manager
+
+package com.cafe.models;
+
+public class Orders{
+
+private Drinks order;
+private Customer customer;
+private Boolean checkOrder;
+
+
+public order(Customer customer, Drinks order){
+    //Orders obj = new Orders(); Where should I create the new object?
+    this.checkOrder = false;
+    this.customer = customer;
+    this.order = order;
+
+}
+
+public Customer getCustomer() {
+        return this.customer;
+    }
+
+public Drinks getOrder() {
+        return this.order;
+    }
+
+public Boolean getCheckOrder(){
+    return this.checkOrder;
+}
+
+//Checks if order is Complete
+public boolean isComplete(){
+    return this.checkOrder = true;
+}
+
+
+}
