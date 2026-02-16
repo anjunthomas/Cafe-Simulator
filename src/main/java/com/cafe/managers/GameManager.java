52f9196494fe78b3885364905b47791ba97d896f
anjunthomas <anjunoblethomas@gmail.com>
2026-02-16 13:02:16 -0500

    added tests for inventory manager in main, they are commented out, but can be uncommented if anyone wants to test to see how it works

--- src/main/java/com/cafe/managers/GameManager.java
+++ src/main/java/com/cafe/managers/GameManager.java
@@ -1,4 +1,29 @@
 package com.cafe.managers;
 
+import com.cafe.models.Drink;
+
 public class GameManager {
+    private int satisfiedCount = 0;
+    private Drink latte;
+    private Drink coffee;
+    private InventoryManager inventoryManager;
+
+    public GameManager(){
+        this.latte = new Drink("Latte", "milk,espresso,cups");
+        this.coffee = new Drink("Coffee", "espresso,cups");
+        this.inventoryManager = new InventoryManager();
+    }
+
+    public int getSatisfiedCount(){
+        return satisfiedCount;
+    }
+
+    // first check if we can make the recipe, then use the recipe
+    public boolean serveDrink(String recipe){
+        boolean success = inventoryManager.useIngredients(recipe);
+        if(success){
+            satisfiedCount++;
+        }
+        return success;
+    }
 }
