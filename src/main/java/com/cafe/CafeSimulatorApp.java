6c85f207d328e595d40edefa0306a0961e8a115e
Salma Petrov <petrovsalma@gmail.com>
2026-02-20 20:39:05 -0500

    Added new ingredients and added glowing/hover effect to all assets

--- src/main/java/com/cafe/CafeSimulatorApp.java
+++ src/main/java/com/cafe/CafeSimulatorApp.java
@@ -1,142 +1,297 @@
 package com.cafe;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 
 import com.cafe.utils.ImageLoader;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.ProgressBar;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.util.Duration;
-
 
 public class CafeSimulatorApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
         boolean[] coffeeBusy = { false };
-        Image background = ImageLoader.load("backgroundReference.png", 800, 800);
-
+        Image background = ImageLoader.load("newbackground.png", 800, 780);
         ImageView backgroundView = new ImageView(background);
 
-        FileInputStream coffee1 = new FileInputStream("C:\\Users\\salma\\OneDrive\\Documents\\GitHub\\Cafe-Simulator\\src\\main\\resources\\images\\coffeebeans.png");
-        Image coffee_full = new Image(coffee1);
 
-        ImageView coffee = new ImageView(coffee_full);
+        /* PLANT */
+
+        Image plant_decor = ImageLoader.load("Plant.png");
+        ImageView plant = new ImageView(plant_decor);
+
+        plant.setTranslateX(280);
+        plant.setTranslateY(-50);
+
+        /* CUPS */
 
         DropShadow glow = new DropShadow();
         glow.setColor(Color.GOLD);
         glow.setRadius(20);
         glow.setSpread(0.6);
 
+        boolean[] pinkBusy = { false };
+        boolean[] redBusy = { false };
+        boolean[] blueBusy = { false };
+
+        Image pink_cup = ImageLoader.load("pinkcup.png");
+        Image red_cup = ImageLoader.load("redcup.png");
+        Image blue_cup = ImageLoader.load("bluecup.png");
+
+        ImageView pinkcup = new ImageView(pink_cup);
+        ImageView redcup = new ImageView(red_cup);
+        ImageView bluecup = new ImageView(blue_cup);
+
+        pinkcup.setTranslateX(-150);
+        pinkcup.setTranslateY(20);
+        redcup.setTranslateX(20);
+        redcup.setTranslateY(20);
+        bluecup.setTranslateX(20);
+        bluecup.setTranslateY(20);
+
+        pinkcup.setOnMouseEntered(e -> {
+            if (!pinkBusy[0]) {
+                pinkcup.setEffect(glow);
+            }
+            else{
+                pinkcup.setEffect(null);
+            }
+        });
+
+        pinkcup.setOnMouseExited(e -> {
+            pinkBusy[0] = false;
+            if (!pinkBusy[0]) {
+                pinkcup.setEffect(null);
+            }
+        });
+
+        pinkcup.setOnMouseClicked(e -> {
+            pinkBusy[0] = true;
+            pinkcup.setEffect(null); 
+
+        });
+
+        StackPane cup_stack = new StackPane(pinkcup, redcup, bluecup);
+
+        /* COFFEE */
+
+        Image coffee_full = ImageLoader.load("coffeebeans.png");
+        ImageView coffee = new ImageView(coffee_full);
+
         coffee.setOnMouseEntered(e -> {
             if (!coffeeBusy[0]) {
                 coffee.setEffect(glow);
             }
+            else{
+                coffee.setEffect(null);
+            }
         });
 
         coffee.setOnMouseExited(e -> {
+            coffeeBusy[0] = false;
             if (!coffeeBusy[0]) {
                 coffee.setEffect(null);
             }
         });
 
+        // ProgressBar coffeeProgress = new ProgressBar(0);
+        // coffeeProgress.setPrefWidth(120); // bigger
+        // coffeeProgress.setPrefHeight(20); // thicker
+        // coffeeProgress.setVisible(false);
+        // coffeeProgress.setStyle("-fx-accent: red;"); // bright color
 
+        StackPane coffeeStack = new StackPane(coffee);
+        // StackPane coffeeStack = new StackPane(coffee, coffeeProgress);
+        // StackPane.setAlignment(coffeeProgress, Pos.CENTER);
 
-        ProgressBar coffeeProgress = new ProgressBar(0);
-        coffeeProgress.setPrefWidth(120);   // bigger
-        coffeeProgress.setPrefHeight(20);   // thicker
-        coffeeProgress.setVisible(false);
-        coffeeProgress.setStyle("-fx-accent: red;"); // bright color
+        coffeeStack.setTranslateX(110);
+        coffeeStack.setTranslateY(60);
 
-        StackPane coffeeStack = new StackPane(coffee, coffeeProgress);
-        StackPane.setAlignment(coffeeProgress, Pos.CENTER);
-
-        coffeeStack.setTranslateX(180);
-        coffeeStack.setTranslateY(40);
+        // Button coffeeButton = new Button();
 
         coffee.setOnMouseClicked(e -> {
 
-            coffeeBusy[0] = true;      // mark busy
-            coffee.setEffect(null);    // remove glow immediately
-            coffee.setDisable(true);  
+            coffeeBusy[0] = true; // mark busy
+            coffee.setEffect(null); 
 
-            coffeeProgress.setProgress(0);
-            coffeeProgress.setVisible(true);
+            // coffeeProgress.setProgress(0);
+            // coffeeProgress.setVisible(true);
 
-            Timeline timeline = new Timeline(
-                    new KeyFrame(Duration.seconds(2),
-                            new KeyValue(coffeeProgress.progressProperty(), 1)
-                    )
-            );
+            // Timeline timeline = new Timeline(
+            // new KeyFrame(Duration.seconds(2),
+            // new KeyValue(coffeeProgress.progressProperty(), 1)
+            // )
+            // );
 
-            timeline.setOnFinished(event -> {
-                coffeeProgress.setVisible(false);
-                coffeeBusy[0] = false;  // allow hover again
-                coffee.setDisable(false);
-            });
-            timeline.play();
+            // timeline.setOnFinished(event -> {
+            // coffeeProgress.setVisible(false);
+            // coffeeBusy[0] = false; // allow hover again
+            // coffee.setDisable(false);
+            // });
+            // timeline.play();
         });
 
-        FileInputStream milk1 = new FileInputStream("C:\\Users\\salma\\OneDrive\\Documents\\GitHub\\Cafe-Simulator\\src\\main\\resources\\images\\milk.png");
-        Image milk_full = new Image(milk1);
-        FileInputStream milk2 = new FileInputStream("C:\\Users\\salma\\OneDrive\\Documents\\GitHub\\Cafe-Simulator\\src\\main\\resources\\images\\milk2.png");
-        Image milk_hover = new Image(milk2);
+        /* MILK */
+
+        boolean[] milkBusy = { false };
+        Image milk_full = ImageLoader.load("milk.png");
         ImageView milk = new ImageView(milk_full);
 
+        milk.setPickOnBounds(false);
+        milk.setTranslateX(-215);
+        milk.setTranslateY(20);
 
-        milk.setPickOnBounds(false); // IMPORTANT
-        milk.setOnMouseEntered(e -> milk.setImage(milk_hover));
-        milk.setOnMouseExited(e -> milk.setImage(milk_full));
-        milk.setTranslateX(-130);
-        milk.setTranslateY(-40);
+        milk.setOnMouseEntered(e -> {
+            if (!milkBusy[0]) {
+                milk.setEffect(glow);
+            }
+            else{
+                milk.setEffect(null);
+            }
+        });
 
-        FileInputStream sugar1 = new FileInputStream("C:\\Users\\salma\\OneDrive\\Documents\\GitHub\\Cafe-Simulator\\src\\main\\resources\\images\\SugarBowl.png");
-        Image sugar_full = new Image(sugar1);
-        FileInputStream sugar2 = new FileInputStream("C:\\Users\\salma\\OneDrive\\Documents\\GitHub\\Cafe-Simulator\\src\\main\\resources\\images\\SugarBowl2.png");
-        Image sugar_hover = new Image(sugar2);
+        milk.setOnMouseExited(e -> {
+            milkBusy[0] = false;
+            if (!milkBusy[0]) {
+                milk.setEffect(null);
+            }
+        });
+
+        milk.setOnMouseClicked(e -> {
+            milkBusy[0] = true;
+            milk.setEffect(null); 
+
+        });
+        
+        /* SUGAR */
+        
+        boolean[] sugarBusy = { false };
+
+        Image sugar_full = ImageLoader.load("SugarBowl.png");
         ImageView sugar = new ImageView(sugar_full);
 
+        sugar.setPickOnBounds(false); 
+        sugar.setTranslateX(-300);
+        sugar.setTranslateY(80);
 
-        sugar.setPickOnBounds(false); // IMPORTANT
-        sugar.setOnMouseEntered(e -> sugar.setImage(sugar_hover));
-        sugar.setOnMouseExited(e -> sugar.setImage(sugar_full));
-        sugar.setTranslateX(-240);
-        sugar.setTranslateY(35);
+        sugar.setOnMouseEntered(e -> {
+            if (!sugarBusy[0]) {
+                sugar.setEffect(glow);
+            }
+            else{
+                sugar.setEffect(null);
+            }
+        });
 
-        FileInputStream cups1 = new FileInputStream("C:\\Users\\salma\\OneDrive\\Documents\\GitHub\\Cafe-Simulator\\src\\main\\resources\\images\\Cups_rack.png");
-        Image cups_stack = new Image(cups1);
-        FileInputStream cups2 = new FileInputStream("C:\\Users\\salma\\OneDrive\\Documents\\GitHub\\Cafe-Simulator\\src\\main\\resources\\images\\Cups_rack2.png");
-        Image cups_hover = new Image(cups2);
+        sugar.setOnMouseExited(e -> {
+            sugarBusy[0] = false;
+            if (!sugarBusy[0]) {
+                sugar.setEffect(null);
+            }
+        });
+
+        sugar.setOnMouseClicked(e -> {
+            sugarBusy[0] = true;
+            sugar.setEffect(null); 
+
+        });
+
+        /* MATCHA */
+        boolean[] matchaBusy = { false };
+
+        Image matcha_full = ImageLoader.load("matchafull.png");
+        ImageView matcha = new ImageView(matcha_full);
+
+        matcha.setPickOnBounds(false); // IMPORTANT
+        matcha.setTranslateX(260);
+        matcha.setTranslateY(70);
+
+        matcha.setOnMouseEntered(e -> {
+            if (!matchaBusy[0]) {
+                matcha.setEffect(glow);
+            }
+            else{
+                matcha.setEffect(null);
+            }
+        });
+
+        matcha.setOnMouseExited(e -> {
+            matchaBusy[0] = false;
+            if (!matchaBusy[0]) {
+                matcha.setEffect(null);
+            }
+        });
+
+        matcha.setOnMouseClicked(e -> {
+            matchaBusy[0] = true;
+            matcha.setEffect(null); 
+
+        });
+
+        /* WATER */
+
+        boolean[] waterBusy = { false };
+
+        Image water_full = ImageLoader.load("waterfull.png");
+        ImageView water = new ImageView(water_full);
+
+        water.setPickOnBounds(false); // IMPORTANT
+        water.setTranslateX(-150);
+        water.setTranslateY(-60);
+
+        water.setOnMouseEntered(e -> {
+            if (!waterBusy[0]) {
+                water.setEffect(glow);
+            }
+            else{
+                water.setEffect(null);
+            }
+        });
+
+        water.setOnMouseExited(e -> {
+            waterBusy[0] = false;
+            if (!waterBusy[0]) {
+                water.setEffect(null);
+            }
+        });
+
+        water.setOnMouseClicked(e -> {
+            waterBusy[0] = true;
+            water.setEffect(null); 
+
+        });
+
+        /* CUPS */
+
+        Image cups_stack = ImageLoader.load("Cups_rack.png");
         ImageView cups = new ImageView(cups_stack);
 
-
         cups.setPickOnBounds(false); // IMPORTANT
-        cups.setOnMouseEntered(e -> cups.setImage(cups_hover));
         cups.setOnMouseExited(e -> cups.setImage(cups_stack));
         cups.setTranslateX(-140);
         cups.setTranslateY(-180);
         cups.setRotate(5);
 
-
         // the StackPane lets us layer images on top of each other
         StackPane root = new StackPane();
         root.getChildren().add(backgroundView);
+        root.getChildren().add(cup_stack);
         root.getChildren().add(coffeeStack);
+        root.getChildren().add(water);
         root.getChildren().add(milk);
         root.getChildren().add(sugar);
         root.getChildren().add(cups);
+        root.getChildren().add(plant);
+        root.getChildren().add(matcha);
+        
+        
 
         Scene scene = new Scene(root, 900, 900);
         stage.setTitle("Cafe Simulator!");
@@ -145,6 +300,49 @@ public class CafeSimulatorApp extends Application {
     }
 
     public static void main(String[] args) {
+        /*
+         * InventoryManager inventory = new InventoryManager(); // this initializes the
+         * ingredients for us: puts them in the hashmap and adds the maxAmount
+         * 
+         * // testing hasIngredient
+         * System.out.println("Has milk: " + inventory.hasIngredient("milk")); // true
+         * System.out.println("Has milk: " + inventory.hasIngredient("xyz")); // false
+         * 
+         * System.out.println("Starting amount of milk: " +
+         * inventory.getIngredient("milk").getCurrentAmount());
+         * System.out.println("Starting amount of espresso: " +
+         * inventory.getIngredient("espresso").getCurrentAmount());
+         * System.out.println("Starting amount of cups: " +
+         * inventory.getIngredient("cups").getCurrentAmount());
+         * 
+         * 
+         * // testing useIngredient works
+         * inventory.useIngredient("milk");
+         * System.out.println("Milk after use: " +
+         * inventory.getIngredient("milk").getCurrentAmount()); // decrementing it from
+         * 10 to 9
+         * 
+         * // Test canMakeRecipe
+         * System.out.println("Can make latte: " +
+         * inventory.canMakeRecipe("milk,espresso,cups")); // should print true, all the
+         * ingredients were initialized when we called the InventoryManager
+         * 
+         * System.out.println("Making latte....!");
+         * // Test useIngredients to make a recipe
+         * inventory.useIngredients("milk,espresso,cups");
+         * System.out.println("Milk after recipe: " +
+         * inventory.getIngredient("milk").getCurrentAmount());
+         * System.out.println("Espresso after recipe: " +
+         * inventory.getIngredient("espresso").getCurrentAmount());
+         * System.out.println("Cups after recipe: " +
+         * inventory.getIngredient("cups").getCurrentAmount());
+         * 
+         * // Test refill
+         * inventory.refillIngredient("milk"); // this will refill the ingredient passed
+         * in to the max amount (10 milk)
+         * System.out.println("Milk after refill: " +
+         * inventory.getIngredient("milk").getCurrentAmount());
+         */
         launch();
     }
 }
